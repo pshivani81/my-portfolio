566bdceaa072b19994d474b85718723d53ddd38f
shivani paidimarri <pshivani_75@yahoo.co.in>
2019-01-23 21:17:47 +0530

    Upload portfolio deployment function  for lambda

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,33 @@
+import json
+import boto3
+from botocore.client import Config
+import StringIO
+import zipfile
+import mimetypes
+def lambda_handler(event, context):
+
+    s3 = boto3.resource('s3',config=Config(signature_version='s3v4'),verify=False)
+
+    portfolio_bucket= s3.Bucket('shivaniportfolio.swipartners.com')
+    build_bucket = s3.Bucket('portfoliobuilde.swipartners.com')
+
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuilde.zip',portfolio_zip)
+
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj =  myzip.open(nm)
+            print nm
+
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj =  myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj,nm)
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return {
+            'statusCode': 200,
+            'body': json.dumps('Hello from Lambda!')
+        }
